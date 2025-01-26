8883f134c5da167e6a7930ca0571484d7f26e5f8
mulberror <mulberror@gmail.com>
2025-01-26 11:24:04 +0800

    lab2: JUnit Tests and Debugging

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,12 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSquarePrimes0() {
+        IntList lst = IntList.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("0 -> 1 -> 4 -> 9 -> 4 -> 25 -> 6 -> 49 -> 8 -> 9 -> 10", lst.toString());
+        assertTrue(changed);
+    }
 }
